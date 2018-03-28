878f07a283face865ce31d0c953edb4ef56b3213
Chase (Ryan Cline) <ryancline88@yahoo.com>
2018-03-28 17:49:12 -0600

    Added pre-loaded data

---
+++ src/main/java/com/chaseclinecoding/project/DatabaseLoader.java
@@ -0,0 +1,23 @@
+package com.chaseclinecoding.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+    private final ProductRepository repository;
+
+    @Autowired
+    public DatabaseLoader(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.repository.save(new Product("Coffee mug", "a white ceramic coffee cup", BigDecimal.valueOf(1.99)));
+    }
+}
